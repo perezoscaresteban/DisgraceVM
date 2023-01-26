7af2576652a582d2e76f3d190c42822c9a8e035f
perezoscaresteban <perezoscaresteban@gmail.com>
2023-01-26 00:28:59 -0300

    VectoresPerezOscarEsteban

---
+++ Assets/Scripts/OutsiderController.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+
+
+
+public enum OutsiderState
+{
+    Idle,
+    Pursuit,
+    RunAway,
+    Seeker
+}
+
+public enum OutsiderRotation { 
+    Direct,
+    Lerp
+}
+
+
+public class OutsiderController : MonoBehaviour
+{
+
+    [SerializeField] private Transform pj;
+    [SerializeField] private OutsiderState currentState;
+    [SerializeField] private float speed;
+    [SerializeField] private float pursuitDistance;
+    [SerializeField] private float rotationSpeed;
+    private Vector3 pjPosition;
+
+    public void SetCurrentState()
+    {
+        switch (currentState)
+        {
+            case OutsiderState.Idle:
+                ExecuteIdle();
+                break;
+            case OutsiderState.Pursuit:
+                ExecutePursuit();
+                break;
+            case OutsiderState.RunAway:
+                ExecuteRunAway();
+                break;
+            case OutsiderState.Seeker:
+                ExecuteSeeker();
+                break;
+            default:
+                Debug.LogError("Current state is invalid");
+                break;
+        }
+    }
+
+    private void ExecuteIdle()
+    {
+        Debug.Log("Idle State");
+    }
+
+    private void ExecutePursuit()
+    {
+        Debug.Log("Pursuit State");
+        var vectorToPJ = pj.position - transform.position;
+        var distance = vectorToPJ.magnitude;
+        if (distance < pursuitDistance)
+        {
+            Quaternion newRotation = Quaternion.LookRotation(pj.position - transform.position);
+            transform.rotation = newRotation;
+            transform.position += vectorToPJ.normalized * (speed * Time.deltaTime);
+        }
+    }
+
+    private void ExecuteRunAway()
+    {
+        Debug.Log("Pursuit RunaWay");
+        var vectorToPJ = transform.position - pj.position;
+        var distance = vectorToPJ.magnitude;
+        if (distance < pursuitDistance)
+        {
+            Quaternion newRotation = Quaternion.LookRotation(pj.position - transform.position);
+            transform.rotation = newRotation;
+            transform.position += vectorToPJ.normalized * (speed * Time.deltaTime);
+        }
+
+    }
+
+    private void ExecuteSeeker()
+    {
+        Debug.Log("Pursuit Seeker");
+        Quaternion newRotation = Quaternion.LookRotation(pj.position - transform.position);
+        transform.rotation = Quaternion.Lerp(transform.rotation,newRotation, Time.deltaTime * rotationSpeed);
+    }
+
+    private void Update()
+    {
+        SetCurrentState();
+    }
+}
